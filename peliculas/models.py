f50ee44daeb337a12300bf9b5d0bae335a2fa15a
Cynthia Castillo <ca.castilloe@gmail.com>
2025-11-13 14:09:46 -0600

    Primer Commit para Cinepedia

---
+++ peliculas/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User #Importamos usuario para poderlo utilizar en Pelicula
+
+class Pelicula(models.Model):
+    nombre = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    fecha_estreno = models.DateField()
+    sinopsis = models.TextField()
+    publicado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+    #comentarios = [Comentario]
+
+    def __str__(self):
+        return self.nombre
+
+class Comentario(models.Model):
+    contenido = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+    pelicula = models.ForeignKey(Pelicula, on_delete=models.CASCADE, related_name='comentarios')
+    autor = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    class Meta:
+        ordering = ['-fecha'] #Por defecto siempre me va a ordenar los más recientes primero
+
+    def __str__(self):
+        return self.contenido
---
+++ usuarios/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+#from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('registro/', views.registro, name='registro'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    #path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+]
